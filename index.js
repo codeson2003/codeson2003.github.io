ba65a7802b8bfb4abd66d30cfb7a97efc7584cd5
Carson Stept <125849175+codeson2003@users.noreply.github.com>
2023-12-27 19:14:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function openResume(){
   // Open the PDF in a new tab
   window.open(pdfUrl, '_blank');
 }
+function openGitHubCode(){
+  var pdfUrl = 'https://github.com/codeson2003/codeson2003.github.io';
+
+  // Open the PDF in a new tab
+  window.open(pdfUrl, '_blank');
+}
